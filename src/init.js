0e4e117f7e5e1252adc7bb77071cadbd7aa3a90d
ShlomoLove <snassau@gmail.com>
2019-01-10 20:19:44 -0800

    update

--- src/init.js
+++ src/init.js
@@ -2,25 +2,10 @@ $(document).ready(function() {
   window.dancers = [];
 
   $('.addDancerButton').on('click', function(event) {
-    /* This function sets up the click handlers for the create-dancer
-     * buttons on dancefloor.html. You should only need to make one small change to it.
-     * As long as the "data-dancer-maker-function-name" attribute of a
-     * class="addDancerButton" DOM node matches one of the names of the
-     * maker functions available in the global scope, clicking that node
-     * will call the function to make the dancer.
-     */
-
-    /* dancerMakerFunctionName is a string which must match
-     * one of the dancer maker functions available in global scope.
-     * A new object of the given type will be created and added
-     * to the stage.
-     */
+ 
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
 
-    // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
-
-    // make a dancer with a random position
 
     var dancer = new dancerMakerFunction(
       $(".arena").height() * Math.random(),
@@ -37,11 +22,12 @@ $(document).ready(function() {
     });
 
     $('.dumbledoreDancer').on('click', function(event) {
+      console.log($(event.currentTarget).attr('src'))
       if($(event.currentTarget).attr('src') === 'Dumbledore2.png') {
-        $(event.currentTarget).removeAttr('src');
+        // $(event.currentTarget).removeAttr('src');
         $(event.currentTarget).attr('src', 'phoenix.png');  
       } else if ($(event.currentTarget).attr('src') === 'phoenix.png') {
-        $(event.currentTarget).removeAttr('src');
+        // $(event.currentTarget).removeAttr('src');
         $(event.currentTarget).attr('src', 'Dumbledore2.png');
       }
     });
@@ -57,10 +43,8 @@ $(document).ready(function() {
     $('.snapeDancer').on('click', function(event) {
       $(event.currentTarget).animate({'top': '-=20'});
     });
-
   })
 
- 
 
   $('.lineUp').on('click', function(event) {
     $.each(window.dancers, function(index) {
